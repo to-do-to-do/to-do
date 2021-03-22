f8a490c43cf8e43bc19193ffb9a882e36d9812b6
M.E. Sparks <msparks@MEs-MacBook-Air.local>
2021-03-22 14:17:49 -0700

    updates

--- to-do/it_all/or_maybe_not/if_someone_else/already_did_it/script.js
+++ to-do/it_all/or_maybe_not/if_someone_else/already_did_it/script.js
@@ -1,9 +1,23 @@
+let imgs = 106;
+let num = [];
+for(i=0;i<imgs;i++){
+  num.push(i);
+}
 window.onload = ()=>{
-  for(i=0;i<82;i++){
+  let k;
+  for(i=0;i<imgs;i++){
+    k = num[Math.floor(Math.random()*num.length)];
+    num.splice(num.indexOf(k),1);
     let img = document.createElement("IMG");
-    img.setAttribute("src","assets/list_"+i+".jpg");
+    img.setAttribute("src","assets/"+k+".jpg");
     img.setAttribute("alt","image"+i);
-    document.getElementById("body").appendChild(img);
+    img.setAttribute("class","slide");
+    img.setAttribute("width","400");
+    if(i%2==0){
+      document.getElementById("column1").appendChild(img);
+    }else{
+      document.getElementById("column2").appendChild(img);
+    }
   }
 
 }
