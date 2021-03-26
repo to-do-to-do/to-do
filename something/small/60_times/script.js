76f1ce98e38979caf17b5fa3ef9c6b8af2f8c979
grunskm <grunskm@gmail.com>
2021-03-25 23:18:17 -0700

    update

--- something/small/60_times/script.js
+++ something/small/60_times/script.js
@@ -1,6 +1,14 @@
 
+let click = [];
 let count = 0;
 let started = false;
+
+function loadSounds(){
+	for(i=0;i<28;i++){
+		click.push(new Audio("samples/click"+i+".mp3"));
+	}
+}
+loadSounds();
 
 window.onload = ()=>{
 	let startbox = document.getElementById("startbox");
@@ -9,6 +17,7 @@ window.onload = ()=>{
 function start(){
 		started = true;
 		document.getElementById("startbox").setAttribute("style","display:none;");
+		//document.getElementById("commandbox").setAttribute("style","display:none;");
 		countDown();
 }
 
@@ -16,11 +25,17 @@ function countDown(){
 	count++;
 	let command = document.getElementById("command");
 	let random = Math.floor(Math.random()*VERBSVB.length);
-	let text = VERBSVB[random];
+	let text = "click";
+
 	if(count<4){
 		text = 4-count;
+	}else if(count>=4){
+		text = VERBSVB[random];
+		click[Math.floor(Math.random()*click.length)].play();
 	}
+
 	command.innerText = text;
+
 	if(count<64){
 		setTimeout(countDown,1000);
 	}else{
@@ -28,8 +43,27 @@ function countDown(){
 		count = 0;
 		started=false;
 		setTimeout(()=>{
-			command.innerText = "click to start";
+			command.innerText = "click";
 			document.getElementById("startbox").setAttribute("style","display:block;");
+			//document.getElementById("commandbox").setAttribute("style","display:block;");
 		},3000);
 	}
+}
+
+function setup(){
+	createCanvas(windowWidth,windowHeight);
+	angleMode(DEGREES);
+	stroke(255,0,255);
+	fill(255,0,255);
+	textSize(20);
+}
+
+function draw(){
+	translate(width/2,height/2);
+	background(30);
+	if(count>=4){
+		rotate((6*(count-4)));
+	}
+	text(command.innerText,-height/3,0);
+	line(0,4,-height/3,4);
 }
